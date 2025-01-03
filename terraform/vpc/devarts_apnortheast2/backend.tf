9d69a6e7d5b187894fcf77923864b91a7687b9ee
LeeEunk <dmsrud7817@naver.com>
2025-01-03 14:17:49 +0900

    s3 modify

--- terraform/vpc/devarts_apnortheast2/backend.tf
+++ terraform/vpc/devarts_apnortheast2/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.5.7"
 
   backend "s3" {
-    bucket         = "devart-preprod-apnortheast2-tfstate"
+    bucket         = "tf101-eunkk-apne2-tfstate"
     key            = "devart/terraform/vpc/devarts_apnortheast2/terraform.tfstate"
     region         = "ap-northeast-2"
     encrypt        = true
