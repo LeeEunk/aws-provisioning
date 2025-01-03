15a2125c2f0636c323ad37c48e52a716c6550d19
LeeEunk <dmsrud7817@naver.com>
2025-01-03 11:44:04 +0900

    stage add

--- terraform/vpc/devarts_apnortheast2/backend.tf
+++ terraform/vpc/devarts_apnortheast2/backend.tf
@@ -1,8 +1,9 @@
 terraform {
+
   required_version = ">= 1.5.7"
 
   backend "s3" {
-    bucket         = "devart-preprod-apnortheast2-tfstate"
+    bucket         = "tf101-eunkk-apne2-tfstate"
     key            = "devart/terraform/vpc/devarts_apnortheast2/terraform.tfstate"
     region         = "ap-northeast-2"
     encrypt        = true
